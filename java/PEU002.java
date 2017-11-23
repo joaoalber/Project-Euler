a942ba08476782d30390ba6f171bfd9099e122e3
João Alberto Rodrigues Rossi <30991248+joaoalber@users.noreply.github.com>
2017-11-23 16:19:02 -0200

    Add files via upload

---
+++ java/PEU002.java
@@ -0,0 +1,16 @@
+public class PEU002 {
+    public static void main (String[]args) {
+        int n1 = 1;
+        int n2 = 1;
+        int n3 = 0;
+        int sumEven = 0;
+        while (n3<=4000000) {
+            n3 = n1+n2;
+            n1 = n2;
+            n2 = n3;
+            if (n3%2==0) 
+                sumEven = n3 + sumEven;
+        }
+        System.out.println ("the sum is: " + sumEven);
+    }
+}
